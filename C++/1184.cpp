be778b27e6b6f512a2e9a045652723cf9a2ecbbf
Nitta Nando Roy <nitta15-6336@s.diu.edu.bd>
2026-03-06 16:37:44 +0600

    Add C++/1184.cpp

---
+++ C++/1184.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fl(n) for (int i = 0; i < n; i++)
+#define flj(n) for (int j = 0; j < n; j++)
+int main()
+{
+  double arr[12][12], sum = 0, avg, cnt = 0;
+  int i, j;
+  char a;
+  cin >> a;
+  for (i = 0; i < 12; i++)
+  {
+    for (j = 0; j < 12; j++)
+    {
+      cin >> arr[i][j];
+      if (i>j)
+      {
+        sum += arr[i][j];
+        cnt++;
+      }
+    }
+  }
+  if (a == 'S')
+    cout <<fixed<<setprecision(1)<< sum << endl;
+  else if (a == 'M')
+    cout << fixed << setprecision(1) << (sum / cnt) << endl;
+  return 0;
+}
