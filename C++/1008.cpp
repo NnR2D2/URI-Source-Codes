fde971a2353736ca2f24191875f4fbcd324cdc3b
Nitta Nando Roy <nitta15-6336@s.diu.edu.bd>
2026-03-06 16:36:03 +0600

    Add C++/1008.cpp

---
+++ C++/1008.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+ int main()
+ {
+
+     int A,B;
+     float X;
+     scanf ("%d%d%f",&A,&B,&X);
+     printf ("NUMBER = %d",A);
+     printf ("\nSALARY = U$ %.2f\n",(B*X));
+    return 0;
+ }
+
