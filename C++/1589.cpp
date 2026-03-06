33dbbf622e8c21c9f105a9c626c58d64f238c18b
Nitta Nando Roy <nitta15-6336@s.diu.edu.bd>
2026-03-06 17:17:53 +0600

    Add C++/1589.cpp

---
+++ C++/1589.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fl(n) for (int i = 0; i < n; i++)
+#define flj(n) for (int j = 0; j < n; j++)
+int main()
+{
+
+int t;
+cin>>t;
+while(t--)
+{
+  int a, b;
+  cin>>a>>b;
+  cout<<(((2*a)+(2*b))/2)<<endl;
+}
+ return 0;
+}
