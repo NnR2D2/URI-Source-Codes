4ab081c758ed2ae64bfd83b0423a7556aabcca8a
Nitta Nando Roy <nitta15-6336@s.diu.edu.bd>
2026-03-06 16:37:53 +0600

    Add C++/1197.cpp

---
+++ C++/1197.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fl(n) for (int i = 0; i < n; i++)
+#define flj(n) for (int j = 0; j < n; j++)
+int main()
+{
+  int a, b;
+  while (cin >> a >> b)
+  {
+    cout << a*b*2 << endl;
+  }
+
+  return 0;
+}
