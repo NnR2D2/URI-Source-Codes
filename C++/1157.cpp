330233c0004c9799efdd84e4e978e25acd21825e
Nitta Nando Roy <nitta15-6336@s.diu.edu.bd>
2026-03-06 17:16:48 +0600

    Add C++/1157.cpp

---
+++ C++/1157.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fl(n) for (int i = 1; i <= n; i++)
+#define flj(n) for (int j = 0; j < n; j++)
+int main()
+{
+
+  ll a, prime = 0;
+  cin >> a;
+  fl(a)
+  {
+    if (a % i == 0)
+    {
+      cout << i << endl;
+    }
+  }
+
+  return 0;
+}
