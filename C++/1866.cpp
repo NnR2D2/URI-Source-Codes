5f75fb6ae318f8a518653b28a94448020dda8d4b
Nitta Nando Roy <nitta15-6336@s.diu.edu.bd>
2026-03-06 16:38:58 +0600

    Add C++/1866.cpp

---
+++ C++/1866.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fl(n) for (int i = 0; i < n; i++)
+#define flj(n) for (int j = 0; j < n; j++)
+int main()
+{
+
+int t;
+cin>>t;
+while(t--)
+{
+ll a;
+cin>>a;
+if(a%2==0)cout<<0<<endl;
+else cout<<1<<endl;
+ 
+
+}
+ return 0;
+}
