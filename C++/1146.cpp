99ec5e54f57ea2e0c83e1bdddb050abb244019c8
Nitta Nando Roy <nitta15-6336@s.diu.edu.bd>
2026-03-06 16:37:20 +0600

    Add C++/1146.cpp

---
+++ C++/1146.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+    int a, i;
+    while (1)
+    {
+        scanf("%d", &a);
+        if (a == 0)
+            break;
+        else
+        {
+            for (i = 1; i <= a; i++)
+            {
+                printf("%d", i);
+                if (i != a)
+                {
+                    printf(" ");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
+
