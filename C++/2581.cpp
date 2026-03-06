bbe2d91fc6aadb008cb0e3a65af14dc369e47e76
Nitta Nando Roy <nitta15-6336@s.diu.edu.bd>
2026-03-06 17:18:56 +0600

    Add C++/2581.cpp

---
+++ C++/2581.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <string.h>
+using namespace std;
+#define ll long long
+#define fl(n) for (int i = 0; i <= n; i++)
+#define flj(n) for (int j = 0; j < n; j++)
+int main()
+{
+
+  int t,i;
+  char str[1000];
+  cin >> t;
+  fl(t)
+  {
+    fgets(str,1000,stdin);
+    if(i!=0)
+    printf( "I am Toorg!\n");
+  }
+  return 0;
+}
