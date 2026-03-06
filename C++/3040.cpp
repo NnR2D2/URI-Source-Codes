5af96a47246a9ed7373b836dc257ae5d0f04cd0e
Nitta Nando Roy <nitta15-6336@s.diu.edu.bd>
2026-03-06 17:19:21 +0600

    Add C++/3040.cpp

---
+++ C++/3040.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fl(n) for (int i = 0; i < n; i++)
+#define flj(n) for (int j = 0; j < n; j++)
+int main()
+{
+
+int t;
+cin>>t;
+while(t--)
+{
+ll i,j,a,b,c;
+cin >> a >> b >> c;
+if(a>=200&&a<=300&&b>=50&&c>=150){
+  cout << "Sim" << endl;
+}
+else cout<<"Nao"<< endl;
+}
+ return 0;
+}
