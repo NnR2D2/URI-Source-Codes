9c39cf1e454012792679e9e3bce22a6ac9aa8caf
Nitta Nando Roy <nitta15-6336@s.diu.edu.bd>
2026-03-06 17:19:18 +0600

    Add C++/2861_2.cpp

---
+++ C++/2861_2.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    double c, less;
+    while (scanf("%d", &a))
+    {
+        while (a--)
+        {
+            printf("gzuz\n");
+        }
+    }
+
+    return 0;
+}
+
